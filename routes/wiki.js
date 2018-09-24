eb4d29ea5ae932325066e18e1f30263f45d87d5e
Sean Ryan <sjr765@gmail.com>
2018-09-24 19:36:56 -0400

    Initial Commit

--- routes/wiki.js
+++ routes/wiki.js
@@ -4,17 +4,6 @@ const router = express.Router();
 
 const { addPage } = require("../views"); // <--- will link to index.js in views folder
 
-router.get("/", (req, res) => {
-  res.send("get at /wiki/");
-});
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("got to POst /wiki/");
-});
-
-router.get("/add", (req, res) => {
-  res.send(addPage());
-});
 
 module.exports = router;
