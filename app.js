21be6b29f48003d1fdaf633a18db7086aebf3af6
Tyler Zoidis <tyler.zoidis@gmail.com>
2018-09-24 19:55:32 -0400

    some middleware

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/public/stylesheets'))
+
 
