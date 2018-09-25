b5dceaf4ef5f4c201ac2e723d5c63788d21b11dd
Tyler Zoidis <tyler.zoidis@gmail.com>
2018-09-24 20:45:31 -0400

    through part 3

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const { db } = require('./models');
 const app = express();
+
+
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/public/stylesheets'))
 
 
+
+const PORT = 3000
+
+(async () => {
+    await db.sync({force: true})
+
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`)
+    })
+  })()
--- models/index.js
+++ models/index.js
@@ -1,9 +1,40 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack', {
+const db = new Sequelize('postgres://localhost:5432/wikistack2', {
     logging: false
 });
-
 
 module.exports = {
  db, Page, User
 }
+
+const Page = db.define('pages', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    }
+})
+
+const User = db.define('users', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        validate: {
+            isEmail: true
+        },
+        allowNull: false
+    }
+})
